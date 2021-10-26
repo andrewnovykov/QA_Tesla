2e1440560b99660aae4af306a1c82a083a6602c2
Andrew <papapin777@gmail.com>
2021-10-25 20:18:44 -0700

    need improve

--- wdio.conf.js
+++ wdio.conf.js
@@ -205,8 +205,9 @@ exports.config = {
    * @param {Array.<String>} specs        List of spec file paths that are to be run
    * @param {Object}         browser      instance of created browser/device session
    */
-  // before: function (capabilities, specs) {
-  // },
+  before: function (capabilities, specs) {
+    //delete json
+  },
   /**
    * Runs before a WebdriverIO command gets executed.
    * @param {String} commandName hook command name

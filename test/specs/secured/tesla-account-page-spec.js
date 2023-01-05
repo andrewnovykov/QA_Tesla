47222b21f66edb8ebaf835b23c65070c5efa3a48
Andrew <papapin777@gmail.com>
2023-01-04 20:53:43 -0800

    done

---
+++ test/specs/secured/tesla-account-page-spec.js
@@ -0,0 +1,10 @@
+export const verify = async (browser) => {
+    browser
+        .url('https://www.tesla.com/account')
+        .waitForElementVisible('body', 1000)
+        .assert.title('Tesla Account')
+        .assert.visible('#email')
+        .assert.visible('#password')
+        .assert.visible('#login')
+        .end();
+}
